e1e2cad58dbb9f3ee4319a44933821b4be75a261
Simon Bates <simon@bitstructures.com>
2017-12-11 15:09:44 -0500

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# nexusctl
+
+Nexus command line tools.
+
+## Usage
+
+### nexusctl-load
+
+Load a JSON configuration into a Nexus.
+
+```
+$ node nexusctl-load.js [options] <file>
+```
+
+#### Available Options
+
+- `-H` or `--host` `<hostname>` Host of Nexus (default: localhost)
+- `-p` or `--port` `<portnumber>` Port number of Nexus (default: 9081)
+- `-h` or `--help` output usage information
---
+++ nexusctl-load.js
@@ -0,0 +1,68 @@
+/*
+Copyright 2017 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://raw.githubusercontent.com/simonbates/nexusctl/master/LICENSE.txt
+*/
+
+"use strict";
+
+var fluid = require("infusion"),
+    fs = require("fs"),
+    gpii = fluid.registerNamespace("gpii"),
+    program = require("commander");
+
+fluid.require("gpii-nexus-client");
+
+// Defaults
+var nexusHost = "localhost";
+var nexusPort = 9081;
+
+program
+    .usage("[options] <file>")
+    .option("-H, --host <hostname>", "Host of Nexus (default: " + nexusHost + ")")
+    .option("-p, --port <portnumber>", "Port number of Nexus (default: " + nexusPort + ")")
+    .parse(process.argv);
+
+if (program.args.length === 0) {
+    console.error("Error: Configuration filename missing");
+    process.exit(1);
+} else if (program.args.length !== 1) {
+    console.error("Error: Please specify exactly one filename");
+    process.exit(1);
+}
+
+var configFilename = program.args[0];
+
+if (program.host) {
+    nexusHost = program.host;
+}
+
+if (program.port) {
+    nexusPort = program.port;
+}
+
+var nexusConfig = JSON.parse(fs.readFileSync(configFilename));
+
+var tasks = [];
+
+fluid.each(nexusConfig.defaults, function (options, gradeName) {
+    tasks.push(function () {
+        return gpii.writeNexusDefaults(nexusHost, nexusPort, gradeName, options);
+    });
+});
+
+fluid.each(nexusConfig.components, function (options, componentPath) {
+    tasks.push(function () {
+        return gpii.constructNexusPeer(nexusHost, nexusPort, componentPath, options);
+    });
+});
+
+fluid.promise.sequence(tasks).then(function () {
+    // SUCCESS
+}, function (error) {
+    console.error(error.message);
+});
